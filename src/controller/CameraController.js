27dc07ed3aa5f2b80e75c82dbac263ef5087a463
Paulo jorge <paulosoujava@gmail.com>
2020-09-08 12:07:42 -0300

    chore: add webpack and correction of camera

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -1,13 +1,13 @@
-class CameraController{
-    constructor(videoEl){
-        this._videoEl = videoEl
-        navigator.mediaDevices.getUserMedia({
-            video: true
-        }).them( stream =>{
-            this._videoEl.src = URL.createObjectURL(stream)
-            this._videoEl.play()
-        }).cathc(er =>{
-            console.log(er)
-        })
-    }
+export class CameraController {
+  constructor (videoEl) {
+    this._videoEl = videoEl
+    navigator.mediaDevices.getUserMedia({
+      video: true
+    }).then(stream => {
+      this._videoEl.srcObject = new MediaStream(stream)
+      this._videoEl.play()
+    }).catch(er => {
+      console.log(er)
+    })
+  }
 }
--- src/util/format.js
+++ src/util/format.js
@@ -1,17 +1,18 @@
-class Format {
-    static getCamelCase(text){
-        let div = document.createElement('div')
-        div.innerHTML = `<div data-${text}="id"></div>`
-        return Object.keys(div.firstChild.dataset)[0]
+export class Format {
+  static getCamelCase (text) {
+    const div = document.createElement('div')
+    div.innerHTML = `<div data-${text}="id"></div>`
+    return Object.keys(div.firstChild.dataset)[0]
+  }
+
+  static toTime (duration) {
+    const secounds = parseInt((duration / 1000) % 60)
+    const minutes = parseInt((duration / (1000 * 60)) % 60)
+    const hours = parseInt((duration / (1000 * 60 * 60)) % 24)
+    if (hours > 0) {
+      return `${hours}:${minutes.toString().padStart(2, '0')}:${secounds.toString().padStart(2, '0')}`
+    } else {
+      return `${minutes}:${secounds.toString().padStart(2, '0')}`
     }
-    static toTime(duration){
-        let secounds = parseInt((duration / 1000) % 60)
-        let minutes = parseInt((duration / (1000 * 60) ) % 60)
-        let hours = parseInt((duration / (1000 * 60 * 60)) % 24)
-        if( hours > 0){
-            return  `${hours}:${minutes.toString().padStart(2,'0')}:${secounds.toString().padStart(2,'0')}`
-        }else{
-            return  `${minutes}:${secounds.toString().padStart(2,'0')}`
-        }
-    }
+  }
 }
